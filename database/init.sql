80c36f593248c1a9f8d8eb0c3b5b77458d96b57d
miahbates <miahbates@icloud.com>
2022-02-01 12:47:52 +0000

    inital file set-up
    Co-authored-by: Oli 78092825+duckRabbitPy@users.noreply.github.com
    Co-authored-by: Adam 31065348+adam8-9@users.noreply.github.com
    Co-authored-by: Juliette <53294140+julietteorpen@users.noreply.github.com>

---
+++ database/init.sql
@@ -0,0 +1,56 @@
+BEGIN;
+
+DROP TABLE IF EXISTS users, posts CASCADE;
+
+-- Create tables and define their columns
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(20),
+  email text UNIQUE NOT NULL,
+  password text NOT NULL
+);
+
+CREATE TABLE posts (
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER,
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  animal_name VARCHAR,
+  type VARCHAR,
+  description VARCHAR,
+  image BYTEA
+);
+
+DROP TABLE IF EXISTS sessions CASCADE;
+
+CREATE TABLE sessions (
+  sid text PRIMARY KEY, 
+  user_info JSON
+);
+
+-- Insert some example data for us to test with
+
+INSERT INTO users (username, email, password) VALUES
+  ('juliettep', 'juliette@juliette.com', '123'),
+  ('olij', 'oli@oli.com', '123')
+;
+
+INSERT INTO posts (animal_name, type, description, image) VALUES
+  ('gus', 'dog', 'friendly whippet', '43435367383'),
+  ('legolas', 'frog', 'long legs', '546372829')
+  
+;
+
+INSERT INTO sessions (sid, user_info) VALUES (
+  'abc123',
+  '{"test":"stuff"}'
+);
+
+COMMIT;
+
+
+
+
+
+
+
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 
 const server = express();
 
+server.use(cookieParser(process.env.COOKIE_SECRET));
+
 const PORT = 3000;
+
+const staticHandler = express.static("public");
+
+server.use(staticHandler);
 
 server.get("/", (request, response) => {
   response.send("hellooooooo");
 });
 
 server.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
+  console.log(`listening on http://localhost:${PORT}`);
 });

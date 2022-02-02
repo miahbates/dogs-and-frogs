93fa175b5b2d44825020ed6a788dd5a309886219
Oli Jones <78092825+duckRabbitPy@users.noreply.github.com>
2022-02-02 12:28:07 +0000

    add clear cookie

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -46,3 +46,33 @@ it("user is redirected to newsfeed page immediately after sign up with new cooki
   cy.url().should("include", "/newsfeed");
   cy.getCookie("sid").should("have.property", "httpOnly", true);
 });
+
+it("user can log in after they have made account", () => {
+  const username = Math.random().toString(36).slice(6);
+  const email = Math.random().toString(36).slice(6);
+  const password = Math.random().toString(36).slice(6);
+  cy.visit("/");
+  cy.get("#sign-up").click();
+  cy.url().should("include", "/signup");
+  cy.get("form").find("input[name='username']").type(`User${username}`);
+  cy.get("form").find("input[name='email']").type(`${email}@gmail.com`);
+  cy.get("form").find("input[name='password']").type(`User${password}`);
+  cy.get("form").find("button[type='submit']").click();
+  cy.url().should("include", "/newsfeed");
+
+  cy.getCookie("sid").should("have.property", "httpOnly", true);
+
+  //remove cookie and then try to log in
+  cy.clearCookies();
+
+  cy.visit("/");
+  cy.get("#log-in").click();
+  cy.url().should("include", "/login");
+
+  cy.get("form").find("input[name='email']").type(`${email}@gmail.com`);
+  cy.get("form").find("input[name='password']").type(`User${password}`);
+  cy.get("form").find("button[type='submit']").click();
+
+  cy.url().should("include", "/newsfeed");
+  cy.getCookie("sid").should("have.property", "httpOnly", true);
+});

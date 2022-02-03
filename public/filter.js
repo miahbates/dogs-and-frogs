90820ed213e7ac402707c4ec82ddcc6c90a1942a
duckRabbitPy <78092825+duckRabbitPy@users.noreply.github.com>
2022-02-03 17:42:14 +0000

    added client side validation for passwords

--- public/filter.js
+++ public/filter.js
@@ -1,5 +1,3 @@
-console.log("hello");
-
 const choices = document.querySelectorAll(".choice");
 const list_items = Array.from(document.querySelectorAll("li"));
 
@@ -8,17 +6,16 @@ choices.forEach((choice) => {
     let chosen_value = e.target.value;
     list_items.filter((li) => {
       const li_type = li.querySelector("p").innerText;
-      console.log("chosen value", chosen_value);
-      console.log("li_type", li_type);
+
       if (li_type !== chosen_value) {
         li.classList.add("hidden");
       } else {
         li.classList.remove("hidden");
       }
 
-      // if(li.childElement){
-      //     consol
-      // }
+      if (chosen_value == "all_animals") {
+        li.classList.remove("hidden");
+      }
     });
   });
 });
--- routes/signup.js
+++ routes/signup.js
@@ -28,7 +28,7 @@ function get(request, response) {
       <label for="email">Email<span aria-hidden="true">*</span></label>
       <input type="email" name="email" required />
       <label for="password">Password<span aria-hidden="true">*</span></label>
-      <input type="password" name="password" required />
+      <input id='password' type="password" name="password" aria-describedby= 'passwordRequired' required pattern='.*\d.*' minlength='8'/>
       <button id="sign-up" class="link" type="submit">Sign up!</button>
     </form>
     <p>Signed up already? Log in below with your details...</p>
